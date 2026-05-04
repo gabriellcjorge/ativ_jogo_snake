4ec8bcacca6714d363acdf7e09147bfb412d69f6
gabriellcjorge <gabriel_c_jorge@estudante.sesisenai.org.br>
2026-05-04 07:49:27 -0300

    Atualização

---
+++ jogo.js
@@ -0,0 +1,93 @@
+const COLUNAS    = 20;
+const LINHAS = 20;
+const  PONTOS_POR_COMIDA = 10;
+
+const VELOCIDADE = parseInt(localStorage.getItem("velocidade_cobra")) || 130;
+
+//Variaveis de estado jogo
+
+let cobra = []; //Armazena a posição da cobra
+let direcao = {x: 1, y: 0} //Direção do movimento
+let proximaDirecao = {x: 1, y: 0} //Direcao da proxima tickada
+let comida = {x: 0, y: 0} //Armazena a posição da comida
+let pontuacao = 0;
+let recorde = parseInt(localStorage.getItem("recorde_cobra")) || 0;
+let intervalo = null; //Referencia do setIntervalo do loop infinito
+let emJogo = false;
+
+//Grade do jogo
+
+const grade = document.getElementById('grade');
+let celulas = []; //Referencia as celulas da grade
+
+
+function criarGrade() {
+    grade.innerHTML = "";
+    celulas = [];
+
+    for(let y = 0; y < LINHAS; y++) {
+        const linha = [];
+
+        for(let x = 0; x < COLUNAS; X++) {
+            const celula = document.createElement("div");
+            celula.className = 'celula';
+            grade.appendChild(celula);
+            linha.push(celula);
+        }
+
+        celulas.push(linha);
+    }
+}
+
+function limparGrade() {
+    for(let y = 0; y < LINHAS; y++) {
+        for (let x = 0; x < COLUNAS; x++) {
+            celulas[y][x].className = "celula";
+        }
+    }
+}
+
+function redenrizar() {
+    limparGrade();
+
+celulas[comida.y][comida.x].classList.add('Comida');
+
+for (let i = 1; i < cobra.length; i++) {
+    celulas[cobra[i].y][cobra[i].x].classList.add("cobra");
+    }
+
+    celulas[cobra[0].y][cobra[0].x].classList.add ("cabeca");
+}
+
+//Inicializao do jooo
+
+function iniciar() {
+    cobra = [
+        {x: 10, y: 10},
+        {x: 9, y: 10},
+        {x: 8, y:10}
+    ];
+
+    direcao = {x: 1, y: 0};
+    proximaDirecao = {x: 1; y: 0};
+    pontuacao = 0;
+    emJogo = true;
+
+    atualizarHUD();
+    gerarComida();
+    renderizar();
+    esconderOverlay();
+
+    //Loop
+    if(intervalo) clearInterval(intervalo);
+
+    intervalo = setInterval(tick, VELOCIDADE);
+}
+
+function reiniciar() {
+    iniciar();
+}
+
+function tik() [
+    
+]
